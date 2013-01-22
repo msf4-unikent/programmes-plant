000c50cfe2ca83b439f62073f90a361415afef69
conatus <alex@recordsonribs.com>
2013-01-22 19:16:08 +0000

    Slightly Cleanup

--- application/models/revision.php
+++ application/models/revision.php
@@ -5,7 +5,6 @@
 */
 class Revision extends Eloquent {
 
-	//Id used to link items of datatype (optional)
 	protected $data_type_id = false;
 
 	/**
@@ -13,7 +12,8 @@ class Revision extends Eloquent {
 	 * 
 	 * @return string identifer 
 	 */
-	public function get_identifier(){
+	public function get_identifier()
+	{
         return "R".$this->{$this->data_type_id}."-{$this->id}";
     }
 
@@ -22,7 +22,8 @@ class Revision extends Eloquent {
 	 * 
 	 * @return string decribing revision
 	 */
-    public function get_identifier_string(){
+    public function get_identifier_string()
+    {
     	$link =  action(URI::segment(1).'/'.URI::segment(2).'/'.URI::segment(3).'/'.$this->{$this->data_type_id}.'@view_revision', array($this->id));
         return '<strong><a href="'.$link.'" target="_blank">'.$this->get_identifier().'</a></strong> created '.$this->get_created_time().' by '.$this->edits_by ;
     }
@@ -32,7 +33,8 @@ class Revision extends Eloquent {
 	 * 
 	 * @return string Published date
 	 */
-    public function get_published_time(){
+    public function get_published_time()
+    {
         return Date("jS F Y \a\\t H:i:s" ,strtotime($this->published_at));
     }
 
@@ -41,7 +43,9 @@ class Revision extends Eloquent {
 	 * 
 	 * @return string creation date
 	 */
-    public function get_created_time(){
+    public function get_created_time()
+    {
         return Date("jS F Y \a\\t H:i:s", strtotime($this->created_at));
     }
+
 }
